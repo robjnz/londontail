770ee47c961fb7c72164fc0875cf89346ad3eaf0
robin harris <robjnz@outlook.com>
2020-03-14 23:36:14 +0000

    remove read more

--- src/components/Theatres/barking.vue
+++ src/components/Theatres/barking.vue
@@ -35,42 +35,7 @@
 
 
      
-<script>
-$(document).ready(function() {
-	var showChar     = 100;
-	var ellipsestext = "...";
-	var moretext     = "read more";
-	var lesstext     = "read less";
-	$('.more').each(function() {
-		var content = $(this).html();
 
-		if(content.length > showChar) {
-
-			var c = content.substr(0, showChar);
-			var h = content.substr(showChar-1, content.length - showChar);
-
-			var html = c + '<span class="moreellipses">' + ellipsestext+ '&nbsp;</span><span class="morecontent"><span>' + h + '</span>&nbsp;&nbsp;<a href="" class="morelink">' + moretext + '</a></span>';
-
-			$(this).html(html);
-		}
-
-	});
-
-	$(".morelink").click(function(){
-		if($(this).hasClass("less")) {
-			$(this).removeClass("less");
-			$(this).html(moretext);
-		} else {
-			$(this).addClass("less");
-			$(this).html(lesstext);
-		}
-		$(this).parent().prev().toggle();
-		$(this).prev().toggle();
-		return false;
-	});
-});
-
-</script>
 <br />
 <g-link
         class  = "text-red-700 hover:text-white"
