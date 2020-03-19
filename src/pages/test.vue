1c25a7823e3210cbdd1b3e15491d4c8fc070f7e5
robin harris <robjnz@outlook.com>
2020-03-19 23:26:12 +0000

    add travel central

--- src/pages/test.vue
+++ src/pages/test.vue
@@ -1,85 +1,198 @@
 <template>
-  <nav class="flex items-center justify-between flex-wrap bg-blue-800 p-6">
-    <div class="flex items-center flex-shrink-0 text-white mr-6">
-      <!--<svg class="fill-current h-8 w-8 mr-2" width="54" height="54" viewBox="0 0 54 54" xmlns="http://www.w3.org/2000/svg"><path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z"/></svg>
-    -->
-      <g-image
-        class = ""
-        src   = "@/images/logowhite_transparent.png" width = "70"
-      ></g-image>
-      <span class="font-semibold text-2xl tracking-tight"
-        ><g-link class="hover:text-red-500" to="/">London East</g-link>
-      </span>
-    </div>
-    <div class="block lg:hidden">
-      <button @click='isOpen = !isOpen'
-        class = "flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white"
+  <Layout>
+    <div id="background" class="m-4 p-4 w-auto rounded-lg">
+      <div class="p-6">
+        <h2 class="text-3xl text-blue-500">
+          <span>
+            <i class="fas fa-subway"></i>
+          </span>
+          Travel
+        </h2> 
+<div class="p-8 w-auto h-25 text-red-600 border-solid border-4 border-red-600 rounded-lg text-left">
+<h5 class="text-center text-red-700 text-2xl">central Line</h5>
+  <g-link
+        class  = "text-red-600 hover:text-white"
+        to     = "http://bit.ly/2TqiECK"
+        target = "_blank"
       >
-        <svg
-          class   = "fill-current h-3 w-3"
-          viewBox = "0 0 20 20"
-          xmlns   = "http://www.w3.org/2000/svg"
-        >
-          <title>Menu</title>
-          <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
-        </svg>
-      </button>
+        Epping
+      </g-link>
+---
+        <g-link
+        class  = "text-red-600 hover:text-white"
+        to     = "http://bit.ly/2TqiECK"
+        target = "_blank"
+      >
+        Theydon Bois
+      </g-link>
+      ---
+       <g-link
+        class  = "text-red-600 hover:text-white"
+        to     = "http://bit.ly/2TqiECK"
+        target = "_blank"
+      >
+        Debden
+      </g-link>
+---
+        <g-link
+        class  = "text-red-600 hover:text-white"
+        to     = "http://bit.ly/2TqiECK"
+        target = "_blank"
+      >
+        Buckhurst Hill
+      </g-link>
+--- 
+   <g-link
+        class  = "text-red-600 hover:text-white"
+        to     = "http://bit.ly/2TqiECK"
+        target = "_blank"
+      >
+        Woodford
+      </g-link>
+---
+        <g-link
+        class  = "text-red-600 hover:text-white"
+        to     = "http://bit.ly/2TqiECK"
+        target = "_blank"
+      >
+        South Woodford
+      </g-link>
+      -----
+        <g-link
+        class  = "text-red-600 hover:text-white"
+        to     = "http://bit.ly/2TqiECK"
+        target = "_blank"
+      >
+        SnaresBrook
+      </g-link>
+---  
+   <g-link
+        class  = "text-red-600 hover:text-white"
+        to     = "http://bit.ly/2TqiECK"
+        target = "_blank"
+      >
+        Leytonstone
+      </g-link>
+---
+        <g-link
+        class  = "text-red-600 hover:text-white"
+        to     = "http://bit.ly/2TqiECK"
+        target = "_blank"
+      >
+        Leyton
+      </g-link>
+      ---
+        <g-link
+        class  = "text-red-600 hover:text-white"
+        to     = "http://bit.ly/2TqiECK"
+        target = "_blank"
+      >
+        Stratford
+      </g-link>
+      ---
+        <g-link
+        class  = "text-red-600 hover:text-white"
+        to     = "http://bit.ly/2TqiECK"
+        target = "_blank"
+      >
+        Mile End
+      </g-link>
       
-        <button @click="isOpen = !isOpen" type="button" class="block text-teal-200 hover:text-white focus:text-white focus:outline-none">
-          <svg class="h-6 w-6 fill-current" viewBox="0 0 24 24">
-            <path v-if="isOpen" fill-rule="evenodd" d="M18.278 16.864a1 1 0 0 1-1.414 1.414l-4.829-4.828-4.828 4.828a1 1 0 0 1-1.414-1.414l4.828-4.829-4.828-4.828a1 1 0 0 1 1.414-1.414l4.829 4.828 4.828-4.828a1 1 0 1 1 1.414 1.414l-4.828 4.829 4.828 4.828z"/>
-            <path v-if="!isOpen" fill-rule="evenodd" d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"/>
-          </svg>
-        </button>
-    </div>
-
-    <div :class="isOpen ? 'block' : 'hidden'  " class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
-      <div class="text-lg lg:flex-grow">
+      <br />
         <g-link
-          to    = "/"
-          class = "block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4"
-        >
-          Home
-        </g-link>
-
+        class  = "text-red-600 hover:text-white"
+        to     = "http://bit.ly/2TqiECK"
+        target = "_blank"
+      >
+        Hainault
+      </g-link>
+      -----
         <g-link
-          to    = "/visit"
-          class = "block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4"
+        class  = "text-red-600 hover:text-white"
+        to     = "http://bit.ly/2TqiECK"
+        target = "_blank"
+      >
+        Fairlop
+      </g-link>
+        -----
+        <g-link
+        class  = "text-red-600 hover:text-white"
+        to     = "http://bit.ly/2TqiECK"
+        target = "_blank"
+      >
+        Barkingside
+      </g-link>
+        -----
+        <g-link
+        class  = "text-red-600 hover:text-white"
+        to     = "http://bit.ly/2TqiECK"
+        target = "_blank"
+      >
+        Newbury Park
+      </g-link>
+        -----
+        <g-link
+        class  = "text-red-600 hover:text-white"
+        to     = "http://bit.ly/2TqiECK"
+        target = "_blank"
+      >
+        Gants Hill
+      </g-link>
+        -----
+        <g-link
+        class  = "text-red-600 hover:text-white"
+        to     = "http://bit.ly/2TqiECK"
+        target = "_blank"
+      >
+        Redbridge
+      </g-link>
+        -----
+        <g-link
+        class  = "text-red-600 hover:text-white"
+        to     = "http://bit.ly/2TqiECK"
+        target = "_blank"
+      >
+       Wanstead
+      </g-link>
+
+       <br />
+        <g-link
+        class  = "text-red-600 hover:text-white"
+        to     = "http://bit.ly/2TqiECK"
+        target = "_blank"
+      >
+        Roding Valley
+      </g-link>
+      -----
+       <g-link
+        class  = "text-red-600 hover:text-white"
+        to     = "http://bit.ly/2TqiECK"
+        target = "_blank"
+      >
+        Chigwell
+      </g-link>
+-----
+ <g-link
+        class  = "text-red-600 hover:text-white"
+        to     = "http://bit.ly/2TqiECK"
+        target = "_blank"
+      >
+        Grange Hill
+      </g-link>
+
+  
+  
+</div>
+        <g-link class="text-blue-500 text-2xl hover:text-white" to="/nightclubs"
+          >More Nightclubs</g-link
         >
-          Places to Visit
-        </g-link>
-        <a
-          href  = "#responsive-header"
-          class = "block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white"
-        >
-          Blog
-        </a>
-      </div>
-
-      <div>
-        <!--  <g-link to="/" class="m-1 inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0">Home</g-link>
-
-            <g-link to="/visit" class="m-1 inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0">Places to Visit</g-link>
-
-            <g-link to="/test" class="m-1 inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0">Sport</g-link>
-
-      <a href="#" class="m-1 inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0">Download</a>
-    -->
       </div>
     </div>
-  </nav>
+  </Layout>
 </template>
+
 <script>
-
 export default {
-  data () {
-    return{
-      isOpen: false,
-    }
-  }
-  }
-
-
-
+  components: {}
+};
 </script>
-<style scoped></style>
