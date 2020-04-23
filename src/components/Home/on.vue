560721ba6ef0b02b28fb01f58b30b4dd608e25a9
robin harris <robjnz@outlook.com>
2020-04-23 05:50:56 +0100

    update date

--- src/components/Home/on.vue
+++ src/components/Home/on.vue
@@ -8,7 +8,7 @@
         <span style="font-size: 1em;">
           <i class="far fa-calendar-alt"></i>
         </span>
-      Wed 22 April 2020
+      Thurs 23 April 2020
       </h3>
       <h1 class="text-red-500  text-2xl">
         <span class="text-pink-300" style="font-size: 1em;">
